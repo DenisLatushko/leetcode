437bb2e6cbc9b0a98eafdbf53817fb092449f53a
DenisLatushko <denis.latushko92@gmail.com>
2023-03-19 17:05:54 +0300

    Resolved TwoSum task

---
+++ src/main/kotlin/algorithms/easy/TwoSum.kt
@@ -0,0 +1,15 @@
+package algorithms.easy
+
+/** https://leetcode.com/problems/two-sum/ **/
+
+fun twoSum(nums: IntArray, target: Int): IntArray {
+    val map = HashMap<Int, Int>()
+    for ((ind, num) in nums.withIndex()) {
+        val possibleValue = target - num
+        if (map.contains(possibleValue)) {
+            return intArrayOf(map[possibleValue]!!, ind)
+        }
+        map[num] = ind
+    }
+    return intArrayOf()
+}
