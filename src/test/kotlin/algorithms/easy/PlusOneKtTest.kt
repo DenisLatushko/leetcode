51f7204cb84b09477790dcb3a467548faddaa6a2
DenisLatushko <denis.latushko92@gmail.com>
2023-04-04 23:30:49 +0300

    Implemented One plus task

---
+++ src/test/kotlin/algorithms/easy/PlusOneKtTest.kt
@@ -0,0 +1,36 @@
+package algorithms.easy
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import kotlin.test.assertEquals
+
+@RunWith(Parameterized::class)
+class PlusOneKtTest(
+    private val number: List<Int>,
+    private val expectedNumber: List<Int>
+) {
+
+    @Test
+    fun `Given number when add 1 then result is bigger on 1`(){
+        val actualResult = plusOne(number.toIntArray())
+
+        assertTrue(expectedNumber.toIntArray() contentEquals actualResult)
+    }
+
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters(
+            name = "Given number {0} when add 1 then result is {1}"
+        )
+        fun data() = listOf(
+            arrayOf(listOf(0), listOf(1)),
+            arrayOf(listOf(1, 2, 3), listOf(1, 2, 4)),
+            arrayOf(listOf(4, 3, 2, 1), listOf(4, 3, 2, 2)),
+            arrayOf(listOf(4, 3, 2, 9), listOf(4, 3, 3, 0)),
+            arrayOf(listOf(9, 9, 9, 9), listOf(1, 0, 0, 0, 0)),
+            arrayOf(emptyList(), emptyList()),
+        )
+    }
+}
