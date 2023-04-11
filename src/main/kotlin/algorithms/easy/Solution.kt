5e16e8486a008c4bc3c8c6196aa6b4d994fecc17
DenisLatushko <denis.latushko92@gmail.com>
2023-04-11 20:09:54 +0300

    Implemented First bad solution

---
+++ src/main/kotlin/algorithms/easy/Solution.kt
@@ -0,0 +1,32 @@
+package algorithms.easy
+
+/** Description: https://leetcode.com/problems/first-bad-version/ **/
+
+abstract class VersionControl(
+    private val badVersion: Int
+) {
+
+    abstract fun firstBadVersion(n: Int) : Int
+
+    fun isBadVersion(version: Int): Boolean {
+        return version >= badVersion
+    }
+}
+
+class Solution(badVersion: Int): VersionControl(badVersion) {
+
+    override fun firstBadVersion(n: Int): Int {
+        var minVersion = 0
+        var maxVersion = n
+        while (minVersion < maxVersion) {
+            val midVersion = minVersion + (maxVersion - minVersion) / 2
+
+            when {
+                isBadVersion(midVersion) -> maxVersion = midVersion
+                else -> minVersion = midVersion + 1
+            }
+        }
+
+        return minVersion
+    }
+}
