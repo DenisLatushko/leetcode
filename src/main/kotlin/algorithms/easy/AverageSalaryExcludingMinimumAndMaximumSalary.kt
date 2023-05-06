e54b6abe8474a54b8563991b1a96341ddedf0acc
DenisLatushko <denis.latushko92@gmail.com>
2023-05-06 13:33:28 +0300

    Implemented Average Salary Excluding the Minimum and Maximum Salary

---
+++ src/main/kotlin/algorithms/easy/AverageSalaryExcludingMinimumAndMaximumSalary.kt
@@ -0,0 +1,20 @@
+package algorithms.easy
+
+import java.lang.Integer.max
+import java.lang.Integer.min
+
+/** https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/ */
+
+fun average(salary: IntArray): Double {
+    var minValue = Int.MAX_VALUE
+    var maxValue = Int.MIN_VALUE
+    var sum = 0
+
+    salary.forEach {
+        sum += it
+        minValue = min(minValue, it)
+        maxValue = max(maxValue, it)
+    }
+
+    return (sum - minValue - maxValue ).toDouble() / (salary.size - 2)
+}
