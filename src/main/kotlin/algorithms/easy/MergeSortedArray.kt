04bb9af3d3ddbf41c583064db3c81d32424b88cd
DenisLatushko <denis.latushko92@gmail.com>
2023-06-29 16:15:19 +0300

    Updated MergeSortedArray task

--- src/main/kotlin/algorithms/easy/MergeSortedArray.kt
+++ src/main/kotlin/algorithms/easy/MergeSortedArray.kt
@@ -11,11 +11,9 @@ fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int) {
         if (ind2 < 0) break
 
         if (ind1 >= 0 && nums1[ind1] >= nums2[ind2]) {
-            nums1[ind3] = nums1[ind1]
-            ind1--
+            nums1[ind3] = nums1[ind1--]
         } else {
-            nums1[ind3] = nums2[ind2]
-            ind2--
+            nums1[ind3] = nums2[ind2--]
         }
 
         ind3--
