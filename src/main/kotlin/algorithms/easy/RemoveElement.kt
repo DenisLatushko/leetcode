44ee2cff3c68f417186f29c933269ab0f27ebddf
DenisLatushko <denis.latushko92@gmail.com>
2023-07-05 23:27:24 +0300

    Implemented RemoveElement

---
+++ src/main/kotlin/algorithms/easy/RemoveElement.kt
@@ -0,0 +1,14 @@
+package algorithms.easy
+
+/** Description: https://leetcode.com/problems/remove-element/ **/
+
+fun removeElement(nums: IntArray, `val`: Int): Int {
+    var ind2 = 0
+    for (ind in 0 .. nums.lastIndex) {
+        if (nums[ind] != `val`) {
+            nums[ind2] = nums[ind]
+            ind2++
+        }
+    }
+    return ind2
+}
